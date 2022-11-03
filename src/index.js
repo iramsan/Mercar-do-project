99b6ce6c207de9210d6e1389a909f30953dec6a4
Iram Sanchez <iramsan0429@gmail.com>
2022-11-03 01:43:17 -0500

    update port

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const storage = multer.diskStorage({
 });
 
 // Settings
-app.set('port', process.env.PORT || 3000);
+app.set('port', /*process.env.PORT*/7009 || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
