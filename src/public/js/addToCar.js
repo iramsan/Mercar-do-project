7b8776cc5ba5e8dfa0f65b03c93dfea9e5aadf54
Iram Sanchez <iramsan0429@gmail.com>
2022-11-25 15:18:38 -0500

    Mercar-do 1.0.2

--- src/public/js/addToCar.js
+++ src/public/js/addToCar.js
@@ -18,17 +18,19 @@ function clickButton(e) {
     const item_name = document.getElementById('artName').textContent;
     const item_price = document.getElementById('artPrice').textContent;
     const item_image = document.getElementById('image_article').src;
+    const item_id_seller = document.getElementById('id_seller').textContent;
 
-    add(createItem(item_id, item_name, item_price, item_image));
+    add(createItem(item_id, item_name, item_price, item_image, item_id_seller));
     location.reload();
 }
 
-function createItem(id, name, price, image) {
+function createItem(id, name, price, image, id_sllr) {
     const item = {
         "id": id,
         "name": name,
         "price": price,
-        "image": image
+        "image": image,
+        "id_seller": id_sllr
     }
     return item
 }
@@ -56,10 +58,10 @@ if (localStorage.getItem('items') !== null) {
         const tr = document.createElement('tr');
         const content = `
             <tr>
-                <td class="BordTable m_table">${acc}</td>
-                <td class="BordTable m_table">${i.name}</td>
-                <td class="BordTable m_table">${i.price}</td>
-                <td class="BordTable m_table"><Button class="delete_art h_color_r bg_color_delete cursor" id="${i.id}" onclick="del(this.id)">Eliminar</Button></td>
+                <td class="BordTable m_table color_w">${acc}</td>
+                <td class="BordTable m_table color_w">${i.name}</td>
+                <td class="BordTable m_table color_w">${i.price}</td>
+                <td class="BordTable m_table color_w"><Button class="delete_art h_color_r bg_color_delete cursor" id="${i.id}" onclick="del(this.id)">Eliminar</Button></td>
             </tr>
         `;
         tr.innerHTML = content;
@@ -92,11 +94,11 @@ document.getElementById('close').addEventListener('click', () => {
 
 
 // Check if the shopping car is empty
-const local_empty = JSON.parse(localStorage.getItem('items'));
+const local_empty = JSON.parse(localStorage.getItem('items')) ? JSON.parse(localStorage.getItem('items')) : [];
 const bttns_empty = document.getElementById('bttns_empty');
 const div_message = document.getElementById('car_empty');
 
-if (local_empty.length !== 0 && local_empty !== null) {
+if (local_empty.length !== 0) {
     div_message.classList.add('inactive');
     bttns_empty.classList.remove('inactive');
 }
--- src/routes/links.js
+++ src/routes/links.js
@@ -5,7 +5,7 @@ const pool = require('../database');
 
 //Routes 
 route.get('/store', async (req, res) => {
-    const article = await pool.query('SELECT ID_article, a.Name as "NameArt" , u.Name as "NameUser", Price, Description, Img, View, Discount, fk_category FROM article as a JOIN users as u ON fk_user = ID_user');
+    const article = await pool.query('SELECT ID_article, a.Name as "NameArt" , u.Name as "NameUser", Price, Description, Img, View, Discount, fk_category FROM article as a JOIN users as u ON fk_user = ID_user ORDER BY ID_article DESC');
     res.render('store', { article });
 });
 
@@ -14,6 +14,13 @@ route.get('/store/article/:id', async (req, res) => {
     const art = await pool.query('SELECT * FROM article WHERE ID_article = ?', [id]);
     const all_data = await pool.query('SELECT ID_article, ID_user, a.Name as "NameArt" , u.Name as "NameUser", Price, Description, Img, View, Discount, fk_category FROM article as a JOIN users as u ON fk_user = ID_user WHERE ID_article = ?', [id]);
     res.render('./partials/article', { art: art[0], all_data: all_data[0] });
+});
+
+route.post('/store/article/:id', (req, res) => {
+    const { id } = req.params;
+    console.log('agregado');
+    req.flash('message_success', '1 Articulo añadido a la compra');
+    res.redirect(`/store/article/${id}`);
 });
 
 route.post('/sell', async (req, res) => {
@@ -137,6 +144,11 @@ route.get('/profile', async (req, res) => {
     if (req.session.user) {
         const Email = req.session.user.Email;
         const data_profile = await pool.query('SELECT ID_article, ID_user, a.Name as "NameArt" , u.Name as "NameUser", Price, Description, Img, View, Discount, fk_category FROM users as u JOIN article as a ON fk_user = ID_user WHERE Email = ?', [Email]);
+        const sales = await pool.query('SELECT * FROM sale_details WHERE Seller_id = ?', [req.session.user.ID_user]);
+        if (sales) {
+            res.render('profile', {data_profile, sales});
+            return null;
+        }
         res.render('profile', {data_profile});
     } else {
         req.flash('message_error', 'Necesitas Iniciar Sesión');
@@ -144,14 +156,71 @@ route.get('/profile', async (req, res) => {
     }
 });
 
-route.post('/store/user/:id', async (req, res) => {
-    await pool.query('');
+route.post('/delete/article/:id', async (req, res) => {
+    const { id } = req.params;
+    await pool.query('DELETE FROM article WHERE ID_article = ?', [id]);
+    req.flash('message_success', 'Articulo eliminado con exito');
     res.redirect('/profile');
 });
 
 route.get('/process-pay', (req, res) => {
-    
     res.render('pay');
+});
+
+route.get('/process-pay/step-one', (req, res) => {
+    if (req.session.user) {
+        let { Direction, Street, Postal_Code, Reference, Tell } = req.body;
+        const errors = [];
+        res.render('partials/step_one', {Direction, Street, Postal_Code, Reference, Tell, errors});
+        return null;
+    }
+    req.flash('message_error', 'Necesitas Iniciar Sesión');
+    res.redirect('/register/login');
+});
+
+route.post('/process-pay/step-one', async (req, res) => {
+    let { Count_arr, arr_ids, arr_sellers, Direction, Street, Postal_Code, Reference, Tell, Credit_card, Credit_name, Credit_date, Credit_code } = req.body;
+    const errors = [];
+    Direction = Direction !== undefined ? Direction : undefined;
+    Street = Street !== undefined ? Street : undefined;
+    Postal_Code = Postal_Code !== undefined ? Postal_Code : undefined;
+    Reference = Reference !== undefined ? Reference : undefined;
+    Tell = Tell !== undefined ? Tell : undefined;
+    // IF empty
+    if (Direction.length == 0 || Street.length == 0 || Postal_Code.length == 0 || Reference.length == 0 || Tell.length == 0 || Credit_card.length == 0 || Credit_name.length == 0 || Credit_date.length == 0 || Credit_code == 0) {
+        errors.push({text: 'Rellena los datos'});
+        res.render('partials/step_one', {Direction, Street, Postal_Code, Reference, Tell, errors});
+        return null;
+    }
+    // Success
+    const user_id = req.session.user.ID_user;
+    const insert = { Direction, Street, Postal_Code, Reference, Tell, User_id_fk : user_id};
+    
+        // if Exist this Direcction
+    const exist = await pool.query('SELECT User_id_fk FROM direction_user');
+    if (exist.length == 0) {
+        await pool.query('INSERT INTO direction_user SET ?', [insert]);
+    } else {
+        await pool.query('UPDATE direction_user SET ? WHERE User_id_fk = ?', [insert, user_id]);
+    }
+    
+    const id_seller = JSON.parse(arr_sellers);
+    const id_article = JSON.parse(arr_ids);
+    for (let i = 0; i < Count_arr; i++) {
+        await pool.query('INSERT INTO sale_details SET Article_id = ?, Seller_id = ?, Buyer_id = ?', [id_article[i].id, id_seller[i].id, user_id]);    
+    }
+    res.send('ok');
+});
+
+route.get('/sales/details', async (req, res) =>{
+    if (req.session.user) {
+        const sales = await pool.query('SELECT * FROM sale_details JOIN article JOIN users ON ID_article = Article_id AND ID_user = Buyer_id WHERE Seller_id = ?', [req.session.user.ID_user]);
+        console.log(sales);
+        res.render('partials/sales_details', {sales});
+        return null;
+    }
+    res.redirect('/register/login');
+
 });
 
 module.exports = route;
